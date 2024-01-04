995ec3ec7df655682c62f28d21d8c2d830c467a0
YuliiaZaharia <juliarybalchenko999@gmail.com>
2024-01-04 14:24:34 +0300

    created function result to show result of quizz

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
-
-
 const button = document.querySelector("#myButton");
 button.addEventListener("click", function() {
 document.querySelector("#myAudio").play();
 })
+
+const btn = document.querySelector("#btn"); 
+btn.addEventListener("click", result); 
+
+const par = document.querySelector("#showResult"); 
+
+function result(e) {
+e.preventDefault();
+
+let points = 0;
+
+if (document.querySelector("#answerOne").checked) {
+    points++   
+}
+
+if (document.querySelector("#answerTwo").checked) {
+    points++
+}
+
+if (document.querySelector("#answerThree").checked) {
+    points++
+}
+
+if (document.querySelector("#answerFour").checked) {
+    points++
+}
+
+if (document.querySelector("#answerFive").checked) {
+    points++
+}
+
+if (document.querySelector("#answerSix").checked) {
+    points++   
+}
+
+if (document.querySelector("#answerSeven").checked) {
+    points++
+}
+
+if (document.querySelector("#answerEight").checked) {
+    points++
+}
+
+if (document.querySelector("#answerNine").checked) {
+    points++
+}
+
+if (document.querySelector("#answerTen").checked) {
+    points++
+}
+
+par.textContent = "Your points: " + points; 
+}
 
 let isRotated = false;
 
