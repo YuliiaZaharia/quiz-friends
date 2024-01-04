240290c846f7c38cb658c112e96b7df11906e4c9
YuliiaZaharia <juliarybalchenko999@gmail.com>
2024-01-04 14:39:59 +0300

    last touches

--- index.js
+++ index.js
@@ -56,17 +56,6 @@ if (document.querySelector("#answerTen").checked) {
 par.textContent = "Your points: " + points; 
 }
 
-let isRotated = false;
-
-document.querySelector('#myButton').addEventListener('click', () => {
-    isRotated = !isRotated;
-    gsap.to('#myButton', {
-      duration: 0.5,
-      rotate: isRotated ? 360 : 0,
-      ease: 'power2.out',
-    });
-  });
-
   gsap.to('.friends', { y: -20, duration: 1, ease: 'bounce.out', repeat: -1, yoyo: true });
  
   gsap.from('h1', {
